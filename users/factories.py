93ce4f26500f64a4896a3eb1f7dc1e4f92c9fc7f
Maciej Izdebski <izdebski.maciek@gmail.com>
2022-05-28 21:16:58 +0200

    OVT-16 Stworzono fabryki dla tabel Person i Users

---
+++ users/factories.py
@@ -0,0 +1,26 @@
+
+from datetime import datetime
+import factory, factory.fuzzy, string
+from factory.django import DjangoModelFactory
+
+class Person(DjangoModelFactory):
+    
+    # Factory-boy wybierze nazwe użytkownika oraz 
+    # imie i nazwisko oraz hasło przy użyciu modułu Faker
+    pesel = factory.fuzzy.FuzzyText(length=11, chars=string.digits)
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    gender = factory.fuzzy.FuzzyChoice(choices=('F','M'))
+    birthdate = factory.fuzzy.FuzzyDate(datetime.date(1980, 1, 1), datetime.date(2003, 1,1))
+
+
+class Users(DjangoModelFactory):
+
+    person = factory.SubFactory(Person)
+    hireDate = factory.fuzzy.FuzzyDate(datetime.date(1998, 1, 1), datetime.date(2003, 1,1))
+    username = factory.Faker('user_name')
+    password = factory.Faker('password')
+    
+    @factory.lazy_attribute
+    def fireDate(self):
+        return factory.fuzzy.FuzzyDate(self.hireDate, datetime.date(2002,12,31)).fuzz
