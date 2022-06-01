f74e34a7555546dd4e0fab245942fb9cf8c5cefc
Maciej Izdebski <izdebski.maciek@gmail.com>
2022-06-01 20:02:57 +0200

    OTV-12 Stworzono model Person

--- users/models.py
+++ users/models.py
@@ -1,4 +1,16 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    GENDER_TYPES = [
+        ('F', 'Kobieta'),
+        ('M', 'Męszczyzna')
+    ]
+    pesel = models.TextField(max_length=11)
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    gender = models.CharField(max_length='1', choices=GENDER_TYPES)
 
