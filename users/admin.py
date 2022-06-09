d71f464b4364bcfb4e06d76a2fe9fd4ce3e8ab18
Maciej Izdebski <izdebski.maciek@gmail.com>
2022-06-09 08:07:30 +0200

    Registred Person model, Added Address model as tabular inline

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,12 @@
 
+from tabnanny import verbose
 from attr import field
 from django.contrib import admin
 from django.forms.models import BaseInlineFormSet, ModelForm
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from users.models import Employee,Person
+from users.models import *
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
@@ -16,21 +17,23 @@ class PersonForm(ModelForm):
         fields = ['pesel', 'first_name', 'last_name', 'birth_date', 'gender']
 
 
-class PersonInline(admin.StackedInline):
-    model = Person
-    can_delete = False
-    fk_name = 'person'
+class AddressInline(admin.TabularInline):
+    model = Address
+    verbose_name_plural = 'adresses'
+    extra = 1
 
-
-class EmployeeInline(admin.options.InlineModelAdmin):
+class EmployeeInline(admin.StackedInline):
     model = Employee
-    fk_name = 'user'
     can_delete = False
     verbose_name_plural = 'employee'
 
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
     inlines = (EmployeeInline,)
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    inlines = (AddressInline,)
 
 # Re-register UserAdmin
 admin.site.unregister(User)
