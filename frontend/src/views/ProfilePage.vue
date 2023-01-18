aff8329b14a34908318fc221ff018f890e38d0f6
Maciej Izdebski <izdebski.maciek@gmail.com>
2023-01-19 00:49:59 +0100

    added showing info on profile page

--- frontend/src/views/ProfilePage.vue
+++ frontend/src/views/ProfilePage.vue
@@ -1,33 +1,55 @@
 <template>
-    <VCard class="mx-auto mt-5" max-width="500px" color="primary-lighten-5">
-        <VCardTitle>Imię Nazwisko</VCardTitle>
+    <VCard class="mx-auto mt-5" max-width="500px" color="primary-lighten-5" v-if="person != null">
+        <VCardTitle>{{ person.first_name + ' ' + person.last_name }}</VCardTitle>
         <VCardText>
-            <VCard class="mb-2">
+            <h3>Płeć: {{ person.gender }}</h3>
+            <h3>Data urodzenia: {{ person.birth_date }}</h3>
+            <h3>PESEL: {{ person.pesel }}</h3>
+
+            <hr style="margin: 30px 0;" color="bg--color-secondary"/>
+
+            <VCard class="mb-2" >
                 <VCardTitle>Telefony</VCardTitle>
-                <VCardText>000-000-000</VCardText>
+                <VCardText>
+                    <p v-for="telephone in telephones" :key="telephone.id">{{ telephone.telephone }}</p>
+                </VCardText>
             </VCard>
-            <VCard class="mb-2">
+            <VCard class="mb-2" >
                 <VCardTitle>Adresy</VCardTitle>
-                <VCardText>000-000-000</VCardText>
+                <VCardText>
+                    <p v-for="address in addresses" :key="address.id">{{ formatAddress(address) }}</p>
+                </VCardText>
             </VCard>
             <VCard class="mb-2">
                 <VCardTitle>Umowy</VCardTitle>
                 <VCardText>Internet + TV + Telefon</VCardText>
             </VCard>
         </VCardText>
-        {{ resp }}
+        
     </VCard>
     
 </template>
 
 <script setup>
-import { inject, ref} from 'vue'
+import { inject, ref } from 'vue'
 
-let resp = ref('');
+let person = ref(null);
+let telephones = ref([]);
+let addresses = ref([]);
 let $axios = inject('axios')
 
-$axios.get("/api/v1/persons/").then((val) => resp.value = JSON.stringify(val,null,4))
+$axios.get("/api/v1/persons/1/")
+      .then((val) => person.value = val.data)
+$axios.get("/api/v1/telephones/")
+      .then((val) => telephones.value = val.data.results, 
+          () => telephones.value = []).then((val) => console.log(val))
+$axios.get("/api/v1/addresses/")
+      .then((val) => addresses.value = val.data.results, 
+          () => addresses.value = []).then((val) => console.log(val))
 
+function formatAddress( { street, house, apartmentNumber }){
+    return `${street} ${house}/${apartmentNumber}`
+}
 
 </script>
 
