92f13f03a63ed69a6e37b6da191ec52f859fe482
Maciej Izdebski <izdebski.maciek@gmail.com>
2023-01-19 00:50:42 +0100

    changed services API to be more restrictive

--- services/views.py
+++ services/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import permissions, mixins
 from . import serializers
 from . import models
 
-class ServiceLocationViewSet(viewsets.ModelViewSet):
+class ServiceLocationViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.ServiceLocation.objects.all()
     serializer_class = serializers.ServiceLocationSerializer
     permission_classes = [permissions.AllowAny]
 
-class OfferViewSet(viewsets.ModelViewSet):
+class OfferViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Offer.objects.all()
     serializer_class = serializers.OfferSerializer
     permission_classes = [permissions.AllowAny]
 
-class ContractViewSet(viewsets.ModelViewSet):
+class ContractViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Contract.objects.all()
     serializer_class = serializers.ContractSerializer
     permission_classes = [permissions.AllowAny]
 
-class DebtViewSet(viewsets.ModelViewSet):
+class DebtViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Debt.objects.all()
     serializer_class = serializers.DebtSerializer
     permission_classes = [permissions.AllowAny]
